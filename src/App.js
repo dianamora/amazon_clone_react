2c0fad238a9f14cc48b151dd1e80a91b6a4a8ae9
Diana Mora <morra.dianna@gmail.com>
2021-04-06 13:50:00 -0400

    imported login

--- src/App.js
+++ src/App.js
@@ -3,47 +3,64 @@ import './App.css';
 import Header from './Header'
 import Cart from './Cart'
 import Home from './Home'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import styled from 'styled-components' 
-import { db } from './firebase'
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import styled from 'styled-components'
+import { db, auth } from './firebase'
+import Login from './Login'
 
 function App() {
-  const [ cartItems, setCartItems ] = useState([]);
-  
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
+  const [cartItems, setCartItems] = useState([]);
+
   const getCartItems = () => {
-    db.collection('cartItems').onSnapshot((snapshot)=>{
+    db.collection('cartItems').onSnapshot((snapshot) => {
       const tempItems = snapshot.docs.map((doc) => ({
         id: doc.id,
         product: doc.data()
       }))
+
       setCartItems(tempItems);
     })
   }
 
-  useEffect(()=>{
+  const signOut = () => {
+      auth.signOut().then(()=>{
+          localStorage.removeItem('user')
+          setUser(null)
+      })
+  }
+
+  useEffect(() => {
     getCartItems();
-    
-  }, []) //need empty array for initial run
+  }, [])
 
 
   return (
     <Router>
-      <Container>
-        <Header />
-        
-        <Switch>
-          
-          <Route path="/cart">
-            <Cart cartItems={cartItems}/>
-          </Route>
-          
-          <Route path="/">
-            <Home />
-          </Route>
-        
-        </Switch>
-      </Container>
+      {
+        !user ? (
+          <Login setUser={setUser} />
+        ) : (
+          <Container>
+            <Header 
+              signOut={signOut}
+              user={user} 
+              cartItems={cartItems} />
+
+            <Switch>
+
+              <Route path="/cart">
+                <Cart cartItems={cartItems} />
+              </Route>
+
+              <Route path="/">
+                <Home />
+              </Route>
+
+            </Switch>
+          </Container>
+        )
+      }
     </Router>
   );
 }
